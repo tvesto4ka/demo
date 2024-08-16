150069c903f5d20d1ff7cdeba7f26e6e19e50f22
tamaratisova <toma-vesta@mail.ru>
2024-08-16 15:50:43 +0500

    truncate tables before test

--- src/test/java/com/example/demo/AbstractSpringTest.java
+++ src/test/java/com/example/demo/AbstractSpringTest.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @Import(TestContainersConfiguration.class)
@@ -11,4 +14,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @AutoConfigureMockMvc
 public abstract class AbstractSpringTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void setUp() throws Exception {
+        jdbcTemplate.update("delete from users");
+        jdbcTemplate.update("UPDATE SQLITE_SEQUENCE SET SEQ=1 WHERE NAME='users'");
+    }
 }
